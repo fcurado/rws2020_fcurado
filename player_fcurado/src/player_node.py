c0df8da15a084275921423fb7b0d2b42db1dee66
fcurado <fjcurado@gmail.com>
2020-02-21 11:21:19 +0000

    actualizado com utils de rws2020_lib

--- player_fcurado/src/player_node.py
+++ player_fcurado/src/player_node.py
@@ -109,6 +109,7 @@ class Player:
 
     def __init__(self, player_name):
         self.player_name = player_name
+        self.listener = tf.TransformListener()
 
         rospy.logwarn('I am ' + self.player_name)
 
